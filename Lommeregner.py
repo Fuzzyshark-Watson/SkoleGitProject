6b218fde4e0a674cc475f9ff1c57210678003c38
Fuzzyshark-Watson <82439482+Fuzzyshark-Watson@users.noreply.github.com>
2022-02-28 12:40:16 +0100

    Watson update

--- Lommeregner.py
+++ Lommeregner.py
@@ -20,13 +20,15 @@ class Lommeregner(object):
         return self.num1 + self.num2
 
     def subtraction(self):
-        return self.num1 + self.num2
+        return self.num1 - self.num2
     
     def multiplication(self):
-        return self.num1 + self.num2
+        return self.num1 * self.num2
     
     def division(self):
-        return self.num1 + self.num2
+        if self.num2 == 0:
+            return 0
+        return self.num1 / self.num2
     
 if __name__ == "__main__":
     obj = Lommeregner(1, 3)
