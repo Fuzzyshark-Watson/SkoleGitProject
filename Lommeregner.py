e2e0cd097add7bc67b893083ed642659a674826e
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-02-28 10:52:16 +0100

    Lommeregner

--- Lommeregner.py
+++ Lommeregner.py
@@ -2,5 +2,19 @@ class Lommeregner(object):
     def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
-        
-        
+    
+    def addition(self):
+        return self.num1 + self.num2
+
+    def subtraction(self):
+        return self.num1 + self.num2
+    
+    def multiplication(self):
+        return self.num1 + self.num2
+    
+    def division(self):
+        return self.num1 + self.num2
+    
+if __name__ == "__main__":
+    obj = Lommeregner(1, 3)
+    print(obj.addition())
